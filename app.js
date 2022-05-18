bef121818c12c9c50e62e8eb9dc82c7e19b6d371
Kevin Friday <kevinfriday87@gmail.com>
2022-05-17 20:46:10 -0500

    updating functions

--- app.js
+++ app.js
@@ -17,7 +17,7 @@
 //       GitHub: ${githubName}
 //     `;
 //   };
-const fs = require('fs');
+const {writeFile,copyFile } = require('./utils/generate-site');
 const inquirer = require('inquirer');
 
 // turns the fucntion  in  page-template in to generatepage
@@ -174,15 +174,45 @@ if (!portfolioData.projects) {
 
 
 promptUser()
+  // .then(promptProject)
+  // .then(portfolioData => {
+  //      const pageHTML = generatePage(portfolioData);
+
+  //   fs.writeFile('./dist/index.html', pageHTML, err => {
+  //     if (err){
+  //       console.log(err)
+  //       return;
+  //     };
+
+  //     console.log('Page created! Check out index.html in this directory to see it!');
+  //     fs.copyFile('./src/style.css', './dist/style.css', err => {
+  //       if (err) {
+  //         console.log(err);
+  //         return;
+  //       }
+  //       console.log('Style sheet copied successfully!');
+  //     });
+
+
+  //   });
+  // });
+
   .then(promptProject)
   .then(portfolioData => {
-       const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
 
 
